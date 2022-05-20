0fbc11bef8923c52fd4435f0d2fd12baaeb76765
Pawel Olszko <p.olszko.p@gmail.com>
2022-05-20 18:18:17 +0200

    Add method which compute lightness of each pixel using HSL color system

--- is-image-bright/src/main/scala/photos.scala
+++ is-image-bright/src/main/scala/photos.scala
@@ -15,6 +15,7 @@ trait PhotoEvaluatingInterface:
     def readPhoto(img: File): BufferedImage
     def savePhoto(img: File): Unit
     def listOfPixels(img: BufferedImage): List[Int]
+    def listOfLightness(pixels: List[Int]): List[Double]
     def evaluating(img: File): Unit
 
 
@@ -31,8 +32,19 @@ object PhotoEvaluating extends PhotoEvaluatingInterface:
     def listOfPixels(img: BufferedImage): List[Int] =
         img.getRGB(0, 0, img.getWidth(), img.getHeight(), null, 0, img.getWidth()).toList
 
+    def listOfLightness(pixels: List[Int]): List[Double] =
+        pixels.map(pixel => {
+            val red = (pixel & 0xff0000) / 65536
+            val green = (pixel & 0xff00) / 256
+            val blue = pixel & 0xff
+            val redPrim: Double = red.toDouble / 255
+            val greenPrim: Double = green.toDouble / 255
+            val bluePrim: Double = blue.toDouble / 255
+            (List(redPrim, greenPrim, bluePrim).max + List(redPrim, greenPrim, bluePrim).min) / 2
+        })
 
     def evaluating(img: File): Unit =
         val photo = readPhoto(img)
-        val pixels = listOfPixels(photo)
+        val pixels = listOfLightness(listOfPixels(photo))
+        // val lightness 
         savePhoto(img)
