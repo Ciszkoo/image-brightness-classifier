c56b9faacf234bb98cf64714f616cf57bc6ba92b
Pawel Olszko <p.olszko.p@gmail.com>
2022-05-22 17:59:48 +0200

    Hopefully last fixes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # recruitment-task
+
+Program wczytuje zdjęcia z folderu podanego w pliku 'applicaton.conf', w nim również znajduje się wartość graniczna do oceny jasności. Ocenia odpowiednio wszystkie zdjęcia załączone do zadania. Aplikację uruchamia sbt run.
--- src/main/scala/photos.scala
+++ src/main/scala/photos.scala
@@ -9,25 +9,26 @@ import scala.collection.parallel.ParSeq
 case class Files(path: String)
 
 object Files:
+    // creates parallel sequence of files to evaluate
     def photosToEvaluate(obj: Files): ParSeq[File] =
         val dir = File(obj.path)
         dir.listFiles.toSeq.par
 
         
 trait PhotoEvaluatingInterface:
-    def readPhoto(img: File): BufferedImage
-    def savePhoto(img: File, lightness: Int): Unit
-    def listOfPixels(img: BufferedImage): ParSeq[Int]
-    def listOfLightness(pixels: ParSeq[Int]): ParSeq[Double]
-    def avgLightness(pixels: ParSeq[Double]): Int
+    protected def readPhoto(img: File): BufferedImage
+    protected def savePhoto(img: File, lightness: Int): Unit
+    protected def listOfPixels(img: BufferedImage): ParSeq[Int]
+    protected def listOfLightness(pixels: ParSeq[Int]): ParSeq[Double]
+    protected def avgLightness(pixels: ParSeq[Double]): Int
     def evaluate(img: File): Unit
 
 
 object PhotoEvaluating extends PhotoEvaluatingInterface:
-    def readPhoto(img: File): BufferedImage =
+    protected def readPhoto(img: File): BufferedImage =
         ImageIO.read(img)
 
-    def savePhoto(img: File, lightness: Int): Unit =
+    protected def savePhoto(img: File, lightness: Int): Unit =
         val fileName = img.toString.split("\\\\").last
         val newName = if (lightness < cutOffPoint) {
             s"${fileName.split("\\.").head}_bright_${lightness}.${fileName.split("\\.").last}"
@@ -39,19 +40,20 @@ object PhotoEvaluating extends PhotoEvaluatingInterface:
             os.Path(s"${outputPath}/${newName}")
         )
     
-    def listOfPixels(img: BufferedImage): ParSeq[Int] =
+    protected def listOfPixels(img: BufferedImage): ParSeq[Int] =
         img.getRGB(0, 0, img.getWidth(), img.getHeight(), null, 0, img.getWidth()).toSeq.par
 
-    def listOfLightness(pixels: ParSeq[Int]): ParSeq[Double] =
+    // get lightness (HSL color system) from RGB value
+    protected def listOfLightness(pixels: ParSeq[Int]): ParSeq[Double] =
         pixels.map(pixel => {
             val c = Color(pixel)
-            val red = c.getRed().toDouble / 255
-            val green = c.getGreen().toDouble / 255
-            val blue = c.getBlue().toDouble / 255
+            val red: Double = c.getRed().toDouble / 255
+            val green: Double = c.getGreen().toDouble / 255
+            val blue: Double = c.getBlue().toDouble / 255
             (List(red, green, blue).max + List(red, green, blue).min) / 2
         })
     
-    def avgLightness(pixels: ParSeq[Double]): Int =
+    protected def avgLightness(pixels: ParSeq[Double]): Int =
         100 - ((pixels.sum / pixels.length.toDouble) * 100).floor.toInt
 
     def evaluate(img: File): Unit =
