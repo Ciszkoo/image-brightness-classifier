64d6dc33a4e3ac93ecd1fe926fe2caed38da79cb
Pawel Olszko <p.olszko.p@gmail.com>
2022-05-22 14:08:13 +0200

    Changes in files structure and in reading paths for input and output folders

--- is-image-bright/src/main/scala/properties.scala
+++
@@ -1,8 +0,0 @@
-import com.typesafe.config._
-
-object Properties {
-    private val conf = ConfigFactory.load()
-    val inputPath = conf.getString("configs.input-path")
-    val outputPath = conf.getString("configs.output-path")
-    val cutOffPoint = conf.getInt("configs.cut-off-point")
-}
