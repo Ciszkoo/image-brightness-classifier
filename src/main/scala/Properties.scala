154952b234b4545c1b7fe13d0544f4bf9c7f949f
Ciszkoo <pawel.olszko.p@gmail.com>
2023-04-08 23:14:53 +0200

    changed config values names

--- src/main/scala/Properties.scala
+++ src/main/scala/Properties.scala
@@ -4,7 +4,7 @@ object Properties {
   private val conf        = ConfigFactory.load()
   private val projectPath = os.pwd.toString
   val fileSeparator       = java.io.File.separator
-  val inputPath: String   = projectPath + fileSeparator + conf.getString("configs.input-path")
-  val outputPath: String  = projectPath + fileSeparator + conf.getString("configs.output-path")
+  val inputPath: String   = projectPath + fileSeparator + conf.getString("configs.input-dir")
+  val outputPath: String  = projectPath + fileSeparator + conf.getString("configs.output-dir")
   val cutOffPoint: Int    = conf.getInt("configs.cut-off-point")
 }
