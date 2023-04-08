df210d6ddc86c33e7b10787b7d543790d772cf39
Ciszkoo <pawel.olszko.p@gmail.com>
2023-04-08 22:30:05 +0200

    added file separator which sholud enable app to work on every os

--- src/main/scala/Properties.scala
+++ src/main/scala/Properties.scala
@@ -1,9 +1,10 @@
 import com.typesafe.config._
 
 object Properties {
-  private val conf = ConfigFactory.load()
+  private val conf        = ConfigFactory.load()
   private val projectPath = os.pwd.toString
-  val inputPath: String = projectPath + "\\" + conf.getString("configs.input-path")
-  val outputPath: String = projectPath + "\\" + conf.getString("configs.output-path")
-  val cutOffPoint: Int = conf.getInt("configs.cut-off-point")
+  val fileSeparator       = java.io.File.separator
+  val inputPath: String   = projectPath + fileSeparator + conf.getString("configs.input-path")
+  val outputPath: String  = projectPath + fileSeparator + conf.getString("configs.output-path")
+  val cutOffPoint: Int    = conf.getInt("configs.cut-off-point")
 }
